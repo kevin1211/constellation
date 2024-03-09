5e222ba9dd92fbc6f0261300bcba2baad7fc2ff3
Guillaume Huysmans <ghuysmans99@gmail.com>
2024-03-09 22:47:21 +0100

    Output a PlantUML sequence diagram on stderr

--- src/Message.java
+++ src/Message.java
@@ -1,6 +1,12 @@
 public class Message extends Event {
     int origin, reply;
     long t1;
+    Node origin_sim;
+
+    @Override
+    public String toString() {
+        return "o=" + origin + ", r=" + reply + ", t1=" + t1;
+    }
 
     public Message(long t) {
         super(t, null);
--- src/World.java
+++ src/World.java
@@ -20,6 +20,7 @@ public class World {
                     int e = (int)Math.hypot(origin.x - dest.x, origin.y - dest.y);
                     int dt = e / 340;
                     Event o;
+                    m.origin_sim = origin;
                     o = m.cloneAt(t + dt);
                     o.dest = dest;
                     events.add(o);
@@ -32,16 +33,24 @@ public class World {
     }
 
     void run() {
+        System.err.println("@startuml");
+        long prev_t = -1; //display t=0
         while (!events.isEmpty()) {
             Event e = events.poll();
             t = e.t_sim;
+            if (t != prev_t)
+                System.out.println("t=" + e.t_sim);
             if (e instanceof PowerOn) {
                 e.dest.onPowerOn();
             }
             else if (e instanceof Message) {
-                e.dest.onMessage((Message)e);
+                Message m = (Message)e;
+                System.err.println(m.origin_sim + " -> " + e.dest + ": " + m);
+                e.dest.onMessage(m);
             }
+            prev_t = t;
         }
+        System.err.println("@enduml");
     }
 
     long getTime() {
