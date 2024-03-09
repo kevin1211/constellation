8bde64f3d354f691988e355d2655d37ce368d720
Guillaume Huysmans <ghuysmans99@gmail.com>
2024-03-09 22:47:04 +0100

    Space nodes

--- src/App.java
+++ src/App.java
@@ -2,16 +2,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         World w = new World();
         Node a, b, c, d;
-        a = new Node(w);
-        b = new Node(w);
-        c = new Node(w);
-        d = new Node(w);
+        a = new Node(w, 0, 0);
+        b = new Node(w, 350, 0);
+        c = new Node(w, 700, 0);
+        d = new Node(w, 1050, 0);
         w.nodes.add(a);
         w.nodes.add(b);
         w.nodes.add(c);
         w.nodes.add(d);
-        w.schedule(new PowerOn(1, a));
-        w.schedule(new PowerOn(2, b));
+        w.schedule(new PowerOn(0, a));
+        w.schedule(new PowerOn(0, b));
+        w.schedule(new PowerOn(0, c));
+        w.schedule(new PowerOn(0, d));
         w.run();
     }
 }
